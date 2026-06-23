bd2871107d3b932522c930da30c1cd820b3ddd45
Ubuntu <ubuntu@ip-172-31-17-224.ec2.internal>
2026-06-23 12:17:03 +0000

    Project Code

--- scripts/permissionexecute.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to grant execute permissions to all shell scripts in the current directory
-#chmod +x jenkins.sh awscli.sh trivy.sh terraform.sh kubectl.sh eksctl.sh prometheus.sh grafana.sh
-
-echo "Making all .sh files executable..."
-chmod +x *.sh
-
-echo "Permissions updated successfully!"
--- terraform/output.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-    value =  "Your Ec2 IP is : ${aws_instance.Monitoring_server.public_ip}"
-}
